3a9de44920efa68e337bc7db350d26c7b01662fa
Sankari04 <n.sankari2004@gmail.com>
2025-12-28 20:01:18 +0530

    Add files via upload

---
+++ objects.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class find{
+    String evenorOdd(int num){
+       if(num%2==0){
+           return "even";
+       } 
+       return "odd";
+    }
+};
+
+public class objects
+{
+	public static void main(String[] args) {
+		
+		Scanner c=new Scanner(System.in);
+		System.out.println("Enter a number:");
+		int num=c.nextInt();
+		find obj=new find();
+		String result=obj.evenorOdd(num);
+		 	System.out.println("result:"+result);
+		
+	}
+}
